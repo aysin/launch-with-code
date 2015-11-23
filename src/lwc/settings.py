66ef568720a94fce9e54d18a58b9fd7d3ccb3dca
Aysin <aysin.oruz@gmail.com>
2015-11-23 21:13:03 +0100

    Adding templates to backend

--- src/lwc/settings.py
+++ src/lwc/settings.py
@@ -75,6 +75,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEMPLATE_DIRS = (
+    # '/Users/aysinoruz/Desktop/lwc/src/templates/',
+    os.path.join(BASE_DIR, 'templates')
+    )
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
--- src/lwc/views.py
+++ src/lwc/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def home(request):
 	context = {}
 	template = 'home.html'
-	return render(request, template, context
+	return render(request, template, context)
